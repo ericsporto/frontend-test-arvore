4bea740f732fcd22ca1a6e93affda0b3a5ef8cc7
Eric de Souza Porto Carvalho <89411680+ericsporto@users.noreply.github.com>
2023-03-04 23:26:35 -0300

    feat: frontend filters without requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import BookField from './components/bookField';
 import Footer from './components/footer';
 import Header from './components/header';
@@ -20,6 +20,17 @@ function App() {
   const [newData, setNewData] = useState<Item[]>([]);
   const loader = useRef(null);
 
+  //States Filters
+  const [money, setMoney] = useState(0);
+  const [money1, setMoney1] = useState(0);
+  const [money2, setMoney2] = useState(0);
+  const [money3, setMoney3] = useState(0);
+  const [available, setAvailable] = useState('');
+  const [available1, setAvailable1] = useState('');
+  const [type, setType] = useState<boolean | null>(null);
+  const [type1, setType1] = useState<boolean | null>(null);
+
+  //Fetch Datas
   const { data: adventureBooks, isLoading: adventureLoading } =
     useFetchBooks('Aventura');
   const { data: actionBooks, isLoading: actionLoading } = useFetchBooks('Acao');
@@ -59,6 +70,44 @@ function App() {
       setNewData((prev) => [...prev, ...booksFiltered?.items]);
     }
   }, [booksFiltered]);
+
+  const filterBooks = useMemo(() => {
+    if (
+      !money &&
+      !money1 &&
+      !money2 &&
+      !money3 &&
+      !available &&
+      !available1 &&
+      !type &&
+      !type1
+    )
+      return newData;
+    return newData.filter((item) => {
+      return (
+        item.saleInfo?.listPrice?.amount < money ||
+        (item.saleInfo?.listPrice?.amount > money &&
+          item.saleInfo?.listPrice?.amount < money1) ||
+        (item.saleInfo?.listPrice?.amount > money1 &&
+          item.saleInfo?.listPrice?.amount < money2) ||
+        item.saleInfo?.listPrice?.amount > money3 ||
+        item.accessInfo.pdf.isAvailable === type1 ||
+        item.accessInfo.pdf.isAvailable === type ||
+        (item.accessInfo.pdf.isAvailable === type &&
+          item.accessInfo.pdf.isAvailable === type1)
+      );
+    });
+  }, [
+    type,
+    type1,
+    newData,
+    money,
+    money1,
+    money2,
+    money3,
+    available,
+    available1,
+  ]);
 
   return (
     <>
@@ -115,13 +164,22 @@ function App() {
               display: 'flex',
             }}
           >
-            <FilterField />
+            <FilterField
+              setType={setType}
+              setType1={setType1}
+              setAvailable={setAvailable}
+              setAvailable1={setAvailable1}
+              setMoney={setMoney}
+              setMoney1={setMoney1}
+              setMoney2={setMoney2}
+              setMoney3={setMoney3}
+            />
             <BookFilteredField
               title={sendSearch}
               tag="h1"
               fontSize="md"
               fontWeight={600}
-              data={newData}
+              data={filterBooks}
             />
           </div>
         </StyledMainContainer>
--- src/components/filterField/index.tsx
+++ src/components/filterField/index.tsx
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 import { StyledText } from '../../styles/typography';
+import { Dispatch, SetStateAction, useState } from 'react';
+
+interface FilterFieldProps {
+  setType: Dispatch<SetStateAction<boolean | null>>;
+  setType1: Dispatch<SetStateAction<boolean | null>>;
+  setAvailable: Dispatch<SetStateAction<string>>;
+  setAvailable1: Dispatch<SetStateAction<string>>;
+  setMoney: Dispatch<SetStateAction<number>>;
+  setMoney1: Dispatch<SetStateAction<number>>;
+  setMoney2: Dispatch<SetStateAction<number>>;
+  setMoney3: Dispatch<SetStateAction<number>>;
+}
 
 export const StyledFilterContainer = styled.div`
   width: 30%;
@@ -16,7 +28,82 @@ export const StyledFilterCheckbox = styled.input`
   height: 15px;
 `;
 
-const FilterField = () => {
+const FilterField: React.FC<FilterFieldProps> = ({
+  setMoney,
+  setMoney1,
+  setMoney2,
+  setMoney3,
+  setAvailable,
+  setAvailable1,
+  setType,
+  setType1,
+}) => {
+  const [checkedMoney, setCheckedMoney] = useState(false);
+  const [checkedMoney1, setCheckedMoney1] = useState(false);
+  const [checkedMoney2, setCheckedMoney2] = useState(false);
+  const [checkedMoney3, setCheckedMoney3] = useState(false);
+  const [checkedAvailable, setCheckedAvailable] = useState(false);
+  const [checkedAvailable1, setCheckedAvailable1] = useState(false);
+  const [checkedType, setCheckedType] = useState(false);
+  const [checkedType1, setCheckedType1] = useState(false);
+
+  const handleChecked = () => {
+    if (!checkedMoney) {
+      setCheckedMoney(true);
+    } else {
+      setCheckedMoney(false);
+    }
+  };
+  const handleChecked1 = () => {
+    if (!checkedMoney1) {
+      setCheckedMoney1(true);
+    } else {
+      setCheckedMoney1(false);
+    }
+  };
+  const handleChecked2 = () => {
+    if (!checkedMoney2) {
+      setCheckedMoney2(true);
+    } else {
+      setCheckedMoney2(false);
+    }
+  };
+  const handleChecked3 = () => {
+    if (!checkedMoney3) {
+      setCheckedMoney3(true);
+    } else {
+      setCheckedMoney3(false);
+    }
+  };
+  const handleChecked4 = () => {
+    if (!checkedAvailable) {
+      setCheckedAvailable(true);
+    } else {
+      setCheckedAvailable(false);
+    }
+  };
+  const handleChecked5 = () => {
+    if (!checkedAvailable1) {
+      setCheckedAvailable1(true);
+    } else {
+      setCheckedAvailable1(false);
+    }
+  };
+  const handleChecked6 = () => {
+    if (!checkedType) {
+      setCheckedType(true);
+    } else {
+      setCheckedType(false);
+    }
+  };
+  const handleChecked7 = () => {
+    if (!checkedType1) {
+      setCheckedType1(true);
+    } else {
+      setCheckedType1(false);
+    }
+  };
+
   return (
     <StyledFilterContainer>
       <StyledText tag="h4" fontSize="l" fontWeight={600}>
@@ -28,26 +115,51 @@ const FilterField = () => {
           Preço
         </StyledText>
       </div>
+
       <StyledFilterBox>
-        <StyledFilterCheckbox type="checkbox" />
+        <StyledFilterCheckbox
+          value={!checkedMoney ? 31.00 : 0}
+          type="checkbox"
+          checked={checkedMoney}
+          onClick={handleChecked}
+          onChange={(e) => setMoney(+e.target.value)}
+        />
         <StyledText tag="h5" fontSize="sm" fontWeight={400}>
           de R$0 até R$30
         </StyledText>
       </StyledFilterBox>
       <StyledFilterBox>
-        <StyledFilterCheckbox type="checkbox" />
+        <StyledFilterCheckbox
+          value={!checkedMoney1 ? 50.00 : 0}
+          type="checkbox"
+          checked={checkedMoney1}
+          onClick={handleChecked1}
+          onChange={(e) => {setMoney1(+e.target.value), setMoney(31.00)}}
+        />
         <StyledText tag="h5" fontSize="sm" fontWeight={400}>
           de R$31 até R$50
         </StyledText>
       </StyledFilterBox>
       <StyledFilterBox>
-        <StyledFilterCheckbox type="checkbox" />
+        <StyledFilterCheckbox
+          value={!checkedMoney2 ? 100 : 0}
+          type="checkbox"
+          checked={checkedMoney2}
+          onClick={handleChecked2}
+          onChange={(e) => {setMoney1(51.00),setMoney2(+e.target.value)}}
+        />
         <StyledText tag="h5" fontSize="sm" fontWeight={400}>
           de R$51 até R$100
         </StyledText>
       </StyledFilterBox>
       <StyledFilterBox>
-        <StyledFilterCheckbox type="checkbox" />
+        <StyledFilterCheckbox
+          value={!checkedMoney3 ? 100 : 0}
+          type="checkbox"
+          checked={checkedMoney3}
+          onClick={handleChecked3}
+          onChange={(e) => setMoney3(+e.target.value)}
+        />
         <StyledText tag="h5" fontSize="sm" fontWeight={400}>
           Mais de R$100
         </StyledText>
@@ -59,13 +171,25 @@ const FilterField = () => {
         </StyledText>
       </div>
       <StyledFilterBox>
-        <StyledFilterCheckbox type="checkbox" />
+        <StyledFilterCheckbox
+        value={!checkedAvailable ? "FOR_SALE" : ''}
+          type="checkbox"
+          checked={checkedAvailable}
+          onClick={handleChecked4}
+          onChange={(e) => setAvailable(e.target.value)}
+        />
         <StyledText tag="h5" fontSize="sm" fontWeight={400}>
           Disponível
         </StyledText>
       </StyledFilterBox>
       <StyledFilterBox>
-        <StyledFilterCheckbox type="checkbox" />
+        <StyledFilterCheckbox
+        value={!checkedAvailable1 ? "NOT_FOR_SALE" : ''}
+          type="checkbox"
+          checked={checkedAvailable1}
+          onClick={handleChecked5}
+          onChange={(e) => setAvailable1(e.target.value)}
+        />
         <StyledText tag="h5" fontSize="sm" fontWeight={400}>
           Indisponível
         </StyledText>
@@ -77,13 +201,23 @@ const FilterField = () => {
         </StyledText>
       </div>
       <StyledFilterBox>
-        <StyledFilterCheckbox type="checkbox" />
+        <StyledFilterCheckbox
+          type="checkbox"
+          checked={checkedType}
+          onClick={handleChecked6}
+          onChange={(e) => setType(e.target.checked)}
+        />
         <StyledText tag="h5" fontSize="sm" fontWeight={400}>
           e-pub
         </StyledText>
       </StyledFilterBox>
       <StyledFilterBox>
-        <StyledFilterCheckbox type="checkbox" />
+        <StyledFilterCheckbox
+          type="checkbox"
+          checked={checkedType1}
+          onClick={handleChecked7}
+          onChange={(e) => setType1(e.target.checked)}
+        />
         <StyledText tag="h5" fontSize="sm" fontWeight={400}>
           PDF
         </StyledText>
